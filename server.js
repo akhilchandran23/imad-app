e15b70711b666d7f28a8086a0ed7009a1a5562c9
Mercury <30392571+akhilchandran23@users.noreply.github.com>
2017-08-05 22:19:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var articleOne = {
                 This is the content for my first article. This is the content for my first article.
                 This is the content for my first article. This is the content for my first article. 
                 This is the content for my first article. This is the content for my first article.  
-                This is the content for my first article. This is the content for my first article. 
+                This is the content for my first article. 
             </p>`
     
 };
