c8ee9822394c2b21dc68f00328218ee8d722502b
Mercury <30392571+akhilchandran23@users.noreply.github.com>
2017-08-07 22:05:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ function moveRight () {
     marginLeft = marginLeft +1;
     img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick = function () {
+img.onclick = function() {
     
     var interval = setInterval(moveRight, 50);
 };
