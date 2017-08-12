6c9eec4d61b67d56da7f6f1802b259a3e73243e1
Mercury <30392571+akhilchandran23@users.noreply.github.com>
2017-08-12 16:59:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,6 +141,19 @@ app.get('/counter', function (req, res) {
 });
 
 
+var names = [];   
+app.get('/submit-name', function(req, res){     // URL: /submit-name?name=xxxxxx
+    //Get the name from the request
+    var name = req.query.name; 
+    
+    names.push(name);
+    // JSON: Javascipt Object Notation
+    res.send(JSON.stringify(names));   // TO DO
+})
+
+
+
+
 app.get('/:articleName', function (req, res) {
     // articleName === article-one
     // articles[articleName] == {} content object for article one
@@ -167,17 +180,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];   
-app.get('/submit-name', function(req, res){     // URL: /submit-name?name=xxxxxx
-    //Get the name from the request
-    var name = req.query.name; 
-    
-    names.push(name);
-    // JSON: Javascipt Object Notation
-    res.send(JSON.stringify(names));   // TO DO
-    
-    
-})
+
 
 
 
