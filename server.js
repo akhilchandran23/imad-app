8860d71f42896b18f75551f044b0e40ced6bdb08
Mercury <30392571+akhilchandran23@users.noreply.github.com>
2017-08-18 14:52:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     user: 'chandranakhil23',
     database: 'chandranakhil23',
     host: 'db.imad.hasura-app.io',
-    port: '5432'
+    port: '5432',
     password: process.env.DB_PASSWORD
 };
 
@@ -148,11 +148,11 @@ var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     // make a select request
     // return a response with the results
-    pool.query('SELEC * FROM test', function (err, result)  {
+    pool.query('SELECT * FROM test', function (err, result)  {
        if (err) {
            res.status(500).send(err.toString());
        } else {
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
            
        }
     });
