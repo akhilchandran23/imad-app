76a7f63678553570c51cef5de6e4a3b0e9b4925e
Mercury <30392571+akhilchandran23@users.noreply.github.com>
2017-08-27 12:47:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,11 @@ submit.onclick = function () {
               //Capture a  list of names and render it as a list
               console.log('user logged in');
               alert('Logged in successfully');
-        } else (request.status === 403) {
-            alert('')
+        } else if(request.status === 403) {
+            alert('Username/Password is incorrect');
+        } else if(request.status === 500) {
+            alert('Something went wrong on the server');
+            
         }
      }
          
@@ -32,10 +35,7 @@ submit.onclick = function () {
    console.log(username);
    console.log(password);
    request.open('POST', 'http://chandranakhil23.imad.hasura-app.io/login',true);
-   request.send(JSON.strinfigy({username: username, password: password}));
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify({username: username, password: password}));
    
-   
-  
-  
-  
 };
